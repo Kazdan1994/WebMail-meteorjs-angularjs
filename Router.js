b1631cf8a2b72514c4a94182ce4615a105db12c8
Kazdan1994 <arthurjacquemin2@gmail.com>
2016-01-18 17:03:26 +0100

    Application qui marche (enfin)

---
+++ Router.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Arthur on 18/01/2016.
+ */
+Router.route('/', {
+    name: 'email'
+})
---
+++ WebMail-meteorjs-angularjs.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Arthur on 18/01/2016.
+ */
+if (Meteor.isClient) {
+    // Ce code sera éxécuté par le client
+    Template.body.helpers({
+        emails: [
+            {
+                expediteur: "carmellamcconnell@velos.com",
+                destinataire: "arthur.jacquemin@mail.com",
+                username: "Hahn",
+                objet: "Do sunt aliqua magna nisi reprehenderit elit cupidatat aliqua enim ex exercitation. Duis aliqua commodo occaecat sint velit aliqua eu incididunt deserunt ipsum.",
+                content: "Aute fugiat consectetur tempor aliquip enim in esse anim quis eu mollit quis enim minim. Ex elit ullamco elit culpa reprehenderit laboris est cupidatat nisi aute sunt Lorem consequat. Quis eu duis laborum anim ea voluptate eu sunt elit. Qui reprehenderit mollit mollit laborum. Consequat adipisicing nostrud esse occaecat ullamco. Dolor occaecat anim irure fugiat sunt amet ad excepteur consectetur culpa esse ut eu. Consequat sunt ullamco culpa veniam id Lorem irure elit et pariatur. Voluptate elit culpa cupidatat officia sint laborum Lorem laboris elit nisi laboris. Sunt minim tempor magna id et ea incididunt laborum velit ad in cupidatat tempor. Incididunt laborum adipisicing reprehenderit amet anim esse incididunt dolor mollit irure id do quis enim. Culpa exercitation proident ad id duis velit aute labore culpa ex. Labore nostrud do et id ea est dolor velit pariatur cupidatat. Laboris laboris cillum elit ullamco pariatur consequat aute ullamco Lorem aliquip. Labore velit excepteur enim non. Dolore nisi nulla sit aliqua ut nostrud sint incididunt. Proident enim nulla nisi nisi cupidatat id qui in. Consectetur sunt in eu consequat. Enim nisi consequat sint qui ipsum nisi. Aliqua eu enim ad est nisi enim ea est. Irure amet elit voluptate qui dolore qui cupidatat minim voluptate non laboris.",
+                date: "25/04/2015"
+            },
+            {
+                expediteur: "ollieclements@ersum.com",
+                destinataire: "arthur.jacquemin@mail.com",
+                username: "Sanford",
+                objet: "Ullamco amet magna eu deserunt minim aliqua proident irure minim irure eiusmod exercitation qui veniam. Irure veniam commodo fugiat reprehenderit officia.",
+                content: "Sit consequat consequat Lorem duis aliqua est pariatur irure tempor incididunt fugiat deserunt eiusmod tempor. Amet nisi ut dolor do do commodo ipsum magna amet incididunt adipisicing officia velit aute. Est elit Lorem nostrud est commodo Lorem aute quis. Cillum amet cupidatat occaecat tempor. Consectetur occaecat mollit pariatur non voluptate sint adipisicing labore veniam veniam exercitation occaecat eiusmod. Aliqua enim id irure ut. Commodo labore nulla non commodo dolore voluptate veniam non esse aute velit et mollit officia. Ipsum id et aute nisi mollit minim sit culpa ullamco irure. Velit ullamco labore irure duis nostrud eu exercitation aute ad voluptate. Nisi do sint tempor irure aliquip commodo labore. Laboris ut enim cupidatat labore elit enim tempor reprehenderit. Aliquip commodo in ad ex consectetur labore incididunt anim labore fugiat cupidatat deserunt. Reprehenderit aliqua mollit incididunt cillum consectetur nostrud consectetur amet laborum. Cupidatat exercitation amet quis cillum elit et veniam voluptate amet enim tempor magna excepteur est. Laboris velit in aliqua tempor eiusmod id eu labore ad aliqua. Dolore consectetur ipsum dolore dolor ipsum proident excepteur Lorem reprehenderit elit. Officia laborum dolore ullamco excepteur nulla sunt irure. Tempor cupidatat sit mollit irure ipsum deserunt aliqua dolor. Est eu aliqua cupidatat laboris labore irure laborum pariatur elit consequat cupidatat. Non laborum minim officia nostrud minim dolor non enim consequat aliqua velit.",
+                date: "14/07/2015"
+            },
+            {
+                expediteur: "castilloschultz@zilodyne.com",
+                destinataire: "arthur.jacquemin@mail.com",
+                username: "Silva",
+                objet: "Dolore anim dolore consectetur labore voluptate consequat id ad veniam sint est. Aliqua nulla aliqua commodo esse ea quis voluptate nulla id dolore dolore ut in sit.",
+                content: "Velit cillum sunt exercitation nulla culpa et enim magna deserunt excepteur nulla. Id deserunt duis ullamco adipisicing irure tempor consectetur eu enim laboris eiusmod magna consequat aute. Amet minim adipisicing quis labore nulla. Et id et veniam esse nostrud proident. Nisi ea commodo qui quis adipisicing veniam minim exercitation excepteur dolor Lorem ea Lorem enim. Eu nisi veniam incididunt officia nostrud anim aliquip in et. Deserunt et aute esse aliquip occaecat irure cupidatat anim do anim. Ea ad duis in esse. Aliquip laborum quis nisi in occaecat tempor duis nulla aliquip. Esse exercitation sint ullamco aliquip pariatur incididunt ut occaecat laborum velit exercitation culpa adipisicing. Id labore laboris laboris consectetur mollit occaecat eu dolor occaecat nulla id occaecat exercitation aliqua. Ipsum fugiat nisi voluptate sunt ut aute velit dolore labore dolore amet. Cupidatat officia ea quis voluptate est laboris exercitation laboris cillum dolore. Officia reprehenderit eu consequat in nostrud exercitation ipsum esse sunt aliqua Lorem aliquip. Non reprehenderit aute do culpa aute commodo esse ipsum voluptate et consectetur deserunt velit. Id qui commodo ad amet occaecat nulla veniam sint et. Quis aliquip aute et commodo veniam in qui. Amet sunt labore sunt nostrud voluptate sit laboris mollit. Amet mollit consectetur sint eiusmod pariatur incididunt aute ex voluptate dolor esse reprehenderit. Est eiusmod sunt sit adipisicing exercitation voluptate duis consectetur exercitation dolor fugiat.",
+                date: "24/05/2015"
+            }
+        ]
+    })
+}
